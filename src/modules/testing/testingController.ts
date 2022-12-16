772b1cbbf01623098a3e82ae6387613bef0ba841
Stanislav <stanislaworonin@yandex.ru>
2022-12-16 09:36:05 +0300

    refactoring not work

--- src/modules/testing/testingController.ts
+++ src/modules/testing/testingController.ts
@@ -1,16 +1,23 @@
-import { Controller, Delete, HttpCode } from '@nestjs/common';
-import { connection } from "mongoose";
+import { Controller, Delete, HttpCode, HttpStatus } from '@nestjs/common';
+import mongoose, { connection } from "mongoose";
 
 @Controller('testing')
 export class TestingController {
   @Delete('all-data')
   @HttpCode(204)
   async deleteAll() {
-    const collections = connection.collections;
-
-    for (const key in collections) {
-      const collection = collections[key];
-      await collection.deleteMany({});
-    }
+    await mongoose.connection.db.dropDatabase();
+    return HttpStatus.NO_CONTENT;
   }
+
+  // @Delete('all-data')
+  // @HttpCode(204)
+  // async deleteAll() {
+  //   const collections = connection.collections;
+  //
+  //   for (const key in collections) {
+  //     const collection = collections[key];
+  //     await collection.deleteMany({});
+  //   }
+  // }
 }
